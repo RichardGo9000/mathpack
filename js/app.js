f54ffba1efbf3bdb9055eaaf885073361052370f
Richard Gorbet <71149500+RichardGo9000@users.noreply.github.com>
2021-01-30 08:38:32 -0700

    trying out 2 IIFEs

--- js/app.js
+++ js/app.js
@@ -1,9 +1,17 @@
 import localforage from 'localforage';
+import Swal from 'sweetalert2';
+
 const save = async(key, value) => localforage.setItem(key, value);
 const load = async(key) => localforage.getItem(key);
+//iife 1 (Immediately Invoked Function Expression)
 (async()=> {
   await save('greeting', 'Hello Rajah');
   const value = await load('greeting');
   document.getElementById('target').textContent = value;
   console.log('done')
+})()
+
+//iife 2
+(() => {
+  console.log("I am cute like Hell...o Kitty.");
 })()
