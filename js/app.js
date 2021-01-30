c453cb5848e5b690af4043c41c061617b2ce71cf
Richard Gorbet <71149500+RichardGo9000@users.noreply.github.com>
2021-01-30 09:03:08 -0700

    console log msg

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ const save = async(key, value) => localforage.setItem(key, value);
 const load = async(key) => localforage.getItem(key);
 //iife 1 (Immediately Invoked Function Expression)
 (async()=> {
+  console.log("I am cute like Hell")
   await save('greeting', 'Hello Rajah');
   const value = await load('greeting');
   document.getElementById('target').textContent = value;
