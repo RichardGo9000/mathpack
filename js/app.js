facbae9bf03b1df01e4b13c9ac933b0456c3b790
Richard Gorbet <71149500+RichardGo9000@users.noreply.github.com>
2021-02-01 10:24:58 -0700

    SweetAlert2 included

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 import localforage from 'localforage';
-// import Swal from 'sweetalert2';
+import Swal from 'sweetalert2';
 
 console.log('begin script');
 
@@ -14,8 +14,29 @@ const load = async(key) => localforage.getItem(key);
   console.log('Async iife complete');
 })()
 
-console.log("meow");
-//iife 2
-(function () {
+;(() => {
   console.log("I am cute like Hell...o Kitty.");
+  // Swal.fire("Sweetest shit you've ever known");
+  Swal.fire({
+    title: 'Are you sure?',
+    text: 'You will not be able to recover this imaginary file',
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonText: 'yes',
+    cancelButtonText: 'No, keep it',
+  }).then((result) => {
+    if (result.value) {
+      Swal.fire(
+        'Deleted',
+        'Your imaginary file has been deleted.',
+        'success'
+      )
+    } else if (result.dismiss === Swal.DismissReason.cancel) {
+      Swal.fire(
+        'Cancelled',
+        'Your imaginary file is safe',
+        'error'
+      )
+    }
+  })
 })()
