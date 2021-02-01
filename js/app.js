c4d65c081b1fbd23d44b814a2d1794e3c264b5d6
Richard Gorbet <71149500+RichardGo9000@users.noreply.github.com>
2021-02-01 10:35:14 -0700

    log error msg

--- js/app.js
+++ js/app.js
@@ -17,27 +17,27 @@ const load = async(key) => localforage.getItem(key);
 // ;(() => {
 (() => {
   console.log("I am cute like Hell...o Kitty.");
-  // Swal.fire("Sweetest shit you've ever known");
-  Swal.fire({
-    title: 'Are you sure?',
-    text: 'You will not be able to recover this imaginary file',
-    icon: 'warning',
-    showCancelButton: true,
-    confirmButtonText: 'yes',
-    cancelButtonText: 'No, keep it',
-  }).then((result) => {
-    if (result.value) {
-      Swal.fire(
-        'Deleted',
-        'Your imaginary file has been deleted.',
-        'success'
-      )
-    } else if (result.dismiss === Swal.DismissReason.cancel) {
-      Swal.fire(
-        'Cancelled',
-        'Your imaginary file is safe',
-        'error'
-      )
-    }
-  })
+  // // Swal.fire("Sweetest shit you've ever known");
+  // Swal.fire({
+  //   title: 'Are you sure?',
+  //   text: 'You will not be able to recover this imaginary file',
+  //   icon: 'warning',
+  //   showCancelButton: true,
+  //   confirmButtonText: 'yes',
+  //   cancelButtonText: 'No, keep it',
+  // }).then((result) => {
+  //   if (result.value) {
+  //     Swal.fire(
+  //       'Deleted',
+  //       'Your imaginary file has been deleted.',
+  //       'success'
+  //     )
+  //   } else if (result.dismiss === Swal.DismissReason.cancel) {
+  //     Swal.fire(
+  //       'Cancelled',
+  //       'Your imaginary file is safe',
+  //       'error'
+  //     )
+  //   }
+  // })
 })()
